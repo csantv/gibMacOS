5733a5c115766d93177dd3fb8dafb2f09c348cdb
csantv <csantve@gmail.com>
2020-06-19 17:29:33 -0500

    add quotes

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,5 +2,5 @@
 
 python3 gibMacOS.command --latest
 DOWNDIR="$PWD/$(find 'macOS Downloads' -type d | tail -1)"
-python3 BuildmacOSInstallApp.command $DOWNDIR
+python3 BuildmacOSInstallApp.command "$DOWNDIR"
 sudo "$DOWNDIR/Install macOS Catalina.app/Contents/Resources/createinstallmedia" --help
